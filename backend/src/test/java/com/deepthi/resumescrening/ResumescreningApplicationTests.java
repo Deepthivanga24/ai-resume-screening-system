46bdf3462701a06e9854f4200244f20b64ed2446
Deepthi Vanga <deepthivanga9@gmail.com>
2026-05-31 05:58:50 +0000

    Added Spring Boot backend

---
+++ backend/src/test/java/com/deepthi/resumescrening/ResumescreningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepthi.resumescrening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumescreningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
